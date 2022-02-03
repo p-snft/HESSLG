867c68b9f34b9fecb34fa05fe8003f141eb8d5ce
Patrik Schönfeldt <git@patrikschoenfeldt.de>
2022-02-03 22:12:36 +0100

    Fetch current status instead of history

--- src/hesslg/vallox/get-data.py
+++ src/hesslg/vallox/get-data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import asyncio
+from pprint import pprint
 from vallox_websocket_api import Client
 
 VALLOX_510_MV_IP = '192.168.178.29'
@@ -8,9 +9,21 @@ VALLOX_510_MV_IP = '192.168.178.29'
 client = Client(VALLOX_510_MV_IP)
 
 
-async def run():
-    data = await client.fetch_raw_logs()
+async def fetch_current():
+    metrics = await client.fetch_metrics([
+        'A_CYC_CO2_VALUE',
+        'A_CYC_EXTRACT_EFFICIENCY',
+        'A_CYC_FAN_SPEED',
+        'A_CYC_HOME_AIR_TEMP_TARGET',
+        'A_CYC_RH_VALUE',
+        'A_CYC_TEMP_EXHAUST_AIR',
+        'A_CYC_TEMP_EXTRACT_AIR',
+        'A_CYC_TEMP_OUTDOOR_AIR',
+        'A_CYC_TEMP_SUPPLY_AIR',
+        'A_CYC_TEMP_SUPPLY_CELL_AIR',
+    ])
+    print(*list(metrics.keys()), sep=", ")
+    print(*list(metrics.values()), sep=", ")
 
-    from pprint import pprint
-    pprint(data)
-asyncio.get_event_loop().run_until_complete(run())
+
+asyncio.run(fetch_current())
