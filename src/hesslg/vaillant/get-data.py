97c8876d557f307f6c9708d95fd02ebe17c27915
Patrik Schönfeldt <patrik@sedna.snft.de>
2022-06-25 18:10:34 +0200

    Add Vaillant cloud readout script

---
+++ src/hesslg/vaillant/get-data.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import aiohttp
+import asyncio
+
+import yaml
+
+from pymultimatic.api import Connector, ApiError, urls
+from pymultimatic.model import mapper
+
+
+async def _get_multimatic_live_data(user, password):
+    async with aiohttp.ClientSession() as sess:
+
+        connector = Connector(user, password, sess)
+
+        try:
+            await connector.login(True)
+        except ApiError as err:
+            print('Cannot login: ' + await err.response.text())
+
+        facilities = await connector.get(urls.facilities_list())
+        serial = mapper.map_serial_number(facilities)
+
+        requests = {}
+        params = {'serial': serial}
+        req = connector.get(urls.live_report(**params))
+        requests.update({urls.live_report.__name__: req})
+
+        responses = {}
+        for key in requests:
+            try:
+                responses.update({key: await requests[key]})
+            except ApiError as api_err:
+                responses.update({key: api_err.response})
+            except err:
+                print('Cannot get response for {}, skipping it'.format(key))
+
+        data = dict()
+
+        for key in responses:
+            reports = responses[key]["body"]["devices"]
+            for report_group in reports:
+                for report in report_group["reports"]:
+                    data[report["name"]
+                         + " ("
+                         + report["unit"]
+                         + ")"] = report["value"]
+
+        return data
+
+
+def get_multimatic_live_data(username, password):
+    return asyncio.get_event_loop().run_until_complete(
+        _get_multimatic_live_data(username, password))
+
+
+def print_multimatic_live_data(username, password):
+    data = get_multimatic_live_data(username, password)
+
+    print(*list(data.keys()), sep=", ")
+    print(*list(data.values()), sep=", ")
+
+
+if __name__ == "__main__":
+    with open("../settings.yaml", "r") as settings_file:
+        settings = yaml.safe_load(settings_file)
+    username = settings["multimatic"]["username"]
+    password = settings["multimatic"]["password"]
+    print_multimatic_live_data(username, password)
+
+
