7541a743a69711304b65bba5b101dd2da059e4ee
Patrik Schönfeldt <patrik@salacia.snft.de>
2023-11-02 22:04:36 +0100

    Save ventilation temperatures to InfluxDB

--- src/hesslg/vallox/get-data.py
+++ src/hesslg/vallox/get-data.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import asyncio
 from vallox_websocket_api import Client
+from influxdb import InfluxDBClient
+
 
 VALLOX_510_MV_IP = '192.168.178.29'
 
@@ -9,7 +11,7 @@ client = Client(VALLOX_510_MV_IP)
 
 
 async def fetch_current():
-    metrics = await client.fetch_metrics([
+    data = await client.fetch_metrics([
         'A_CYC_CO2_VALUE',
         'A_CYC_EXTRACT_EFFICIENCY',
         'A_CYC_FAN_SPEED',
@@ -21,8 +23,36 @@ async def fetch_current():
         'A_CYC_TEMP_SUPPLY_AIR',
         'A_CYC_TEMP_SUPPLY_CELL_AIR',
     ])
-    print(*list(metrics.keys()), sep=", ")
-    print(*list(metrics.values()), sep=", ")
 
 
-asyncio.run(fetch_current())
+    client = InfluxDBClient(database='hesslg')
+
+    def _datapoint_dict(label):
+        datapoint_dict = {
+                "measurement": label,
+                "tags": {
+                    "type": "measurement",
+                    "source": "vallox"
+                },
+                "fields": {
+                    "value": data[label]
+                }
+            }
+        return datapoint_dict
+
+    json_body = [
+        _datapoint_dict("A_CYC_TEMP_EXHAUST_AIR"),
+        _datapoint_dict("A_CYC_TEMP_EXTRACT_AIR"),
+        _datapoint_dict("A_CYC_TEMP_OUTDOOR_AIR"),
+        _datapoint_dict("A_CYC_TEMP_SUPPLY_AIR"),
+        _datapoint_dict("A_CYC_TEMP_SUPPLY_CELL_AIR")
+    ]
+
+    client.write_points(json_body)
+
+    print(*list(data.keys()), sep=", ")
+    print(*list(data.values()), sep=", ")
+
+
+if __name__ == "__main__":
+    asyncio.run(fetch_current())
