6462a55d40f9647dc3354e495af959c0f3a59ff1
Patrik Schönfeldt <patrik@salacia.snft.de>
2024-02-10 21:01:13 +0100

    Log DC values of Kostal Pico

--- src/hesslg/kostal_pico/interpret-pico-pv-data.py
+++ src/hesslg/kostal_pico/interpret-pico-pv-data.py
@@ -17,30 +17,42 @@ JSON_FILE_BASENAME=sys.argv[1][:-5]
 
 with open(JSON_FILE_BASENAME+".json", 'r') as json_file:
     data = json.load(json_file)
-    
-produced_date = datetime.strptime(
-    data['DayCurves']['Datasets'][0]['Data'][0]['Timestamp'],
-    "%Y-%m-%d")
-produced_step = data['DayCurves']['Datasets'][0]['Data'][0]['Offset']
-produced_hour = produced_step//6
-produced_minute = produced_step%6*10
 
-produced_start = produced_date + timedelta(hours = produced_hour,
-                                            minutes = produced_minute)
+unit = data['DayCurves']['Unit']
+data = data['DayCurves']['Datasets']
 
-produced_data = np.array(data['DayCurves']['Datasets'][0]['Data'][0]['Data'])
-produced_time = [produced_start + i*timedelta(minutes=10)
-                    for i in range(len(produced_data))]
+produced_df = list()
+for dataset in data:
+    title = dataset["Type"]
 
-produced_df = pd.DataFrame(produced_data,
-                            columns=['produced (W)'],
-                            index=produced_time)
+    dataset = dataset['Data'][0]
 
+    produced_date = datetime.strptime(
+        dataset['Timestamp'],
+        "%Y-%m-%d")
+    produced_step = dataset['Offset']
+    produced_hour = produced_step//6
+    produced_minute = produced_step%6*10
 
+    produced_start = produced_date + timedelta(hours = produced_hour,
+                                                minutes = produced_minute)
+
+    produced_data = np.array(dataset['Data'])
+    produced_time = [produced_start + i*timedelta(minutes=10)
+                        for i in range(len(produced_data))]
+
+    produced_df.append(pd.DataFrame(
+        produced_data,
+        columns=[f'{title} ({unit})'],
+        index=produced_time,
+    ))
+
+produced_df = pd.concat(produced_df, axis=1)
+
+produced_df.fillna(0, inplace=True)
 
 produced_df.to_csv(JSON_FILE_BASENAME+".csv",
-                    date_format='%Y-%m-%dT%H:%M:%S')
-
+                   date_format='%Y-%m-%dT%H:%M:%S')
 
 client = InfluxDBClient(database='hesslg')
 
@@ -54,7 +66,10 @@ def store_piko_live_data(data):
                 "source": "kostal pico"
             },
             "fields": {
-                "value": point[1][0]
+                "value": point[1][0],
+                "PV1": point[1][1],
+                "PV2": point[1][2],
+
             },
             "time": point[0].tz_localize(tz='Europe/Berlin')
         }
